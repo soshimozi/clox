0c91f864fefd2ab104b1fa472b9181cb7e9fdd1e
Scott McCain <scott.mccain@gmail.com>
2022-06-21 12:50:18 -0700

    Completed for statement

--- src/compiler.c
+++ src/compiler.c
@@ -438,6 +438,51 @@ static void expressionStatement() {
 	emitByte(OP_POP);
 }
 
+static void forStatement() {
+	beginScope();
+	consume(TOKEN_LEFT_PAREN, "Expected '(' after 'for'.");
+	if (match(TOKEN_SEMICOLON)) {
+		// no initalizer
+	}
+	else if (match(TOKEN_VAR)) {
+		varDeclaration();
+	} else {
+		expressionStatement();
+	}
+
+	int loopStart = currentChunk()->count;
+	int exitJump = -1;
+	if (!match(TOKEN_SEMICOLON)) {
+		expression();
+		consume(TOKEN_SEMICOLON, "Expected ');' after loop condition'");
+
+		// Jump out of the lop if the condition is false
+		exitJump = emitJump(OP_JUMP_IF_FALSE);
+		emitByte(OP_POP);	// Condition
+	}
+
+	if( !match(TOKEN_RIGHT_PAREN)) {
+		const int bodyJump = emitJump(OP_JUMP);
+		const int incrementStart = currentChunk()->count;
+		expression();
+		emitByte(OP_POP);
+		consume(TOKEN_RIGHT_PAREN, "Expected ')' after for clauses.");
+
+		emitLoop(loopStart);
+		loopStart = incrementStart;
+		patchJump(bodyJump);
+	}
+
+	statement();
+	emitLoop(loopStart);
+	if (exitJump != -1) {
+		patchJump(exitJump);
+		emitByte(OP_POP);
+	}
+
+	endScope();
+}
+
 static void ifStatement() {
 	consume(TOKEN_LEFT_PAREN, "Expected '(' after if.");
 	expression();
@@ -515,6 +560,9 @@ static void declaration() {
 static void statement() {
 	if (match(TOKEN_PRINT)) {
 		printStatement();
+	}
+	else if (match(TOKEN_FOR))	{
+		forStatement();
 	}
 	else if (match(TOKEN_IF)) {
 		ifStatement();
